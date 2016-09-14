88868b65754c028e72c7bd1be1e4aa14e35172d2
Libra1002 <kzhen5175@bths.edu>
2016-09-14 13:51:47 -0400

    Update ProceduralExample.java

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -1,7 +1,7 @@
 package introduction;
 
 public class ProceduralExample {
-public static void main(String [] args){
+public static void main(String[]args){
   System.out.println("Hello World;");
   }
 }
