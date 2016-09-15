5d96220b5ea81cbce28c0deafaf0d193114af1db
Student8 <Student8@10.8.33.161>
2016-09-15 14:01:51 -0400

    spelling error

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -8,8 +8,8 @@ public static void main(String[]args){
  
 	String[] names={"Jillian", "Joseph", "Jordan"};
 	String[] years={"Freshman", "Sophomore", "Junior", "Senior"};
-	System.out.println("Hi, ny name is "+names[0]+ ". I am in my " +years[0]+ " year.");
-	System.out.println("Hi, ny name is "+names[1]+ ". I am in my " +years[2]+ " year.");
-	System.out.println("Hi, ny name is "+names[2]+ ". I am in my " +years[3]+ " year.");
+	System.out.println("Hi, my name is "+names[0]+ ". I am in my " +years[0]+ " year.");
+	System.out.println("Hi, my name is "+names[1]+ ". I am in my " +years[2]+ " year.");
+	System.out.println("Hi, my name is "+names[2]+ ". I am in my " +years[3]+ " year.");
   }
 }
