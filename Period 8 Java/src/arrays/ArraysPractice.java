3d7cd11851c4d6cd32fd153bf85ad3250fb6b98b
Student8 <Student8@10.8.33.161>
2016-10-14 14:03:51 -0400

    practice

--- Period 8 Java/src/arrays/ArraysPractice.java
+++ Period 8 Java/src/arrays/ArraysPractice.java
@@ -2,6 +2,7 @@ package arrays;
 
 public class ArraysPractice 
 {
+		static boolean[] boos3;
 		public static void main(String[] args) 
 		{
 			/*arrays are kind of like lists
@@ -16,5 +17,48 @@ public class ArraysPractice
 			boolean[] boos1 = new boolean[3];
 			//can ONLY be done at the declaration because it sets size and content
 			boolean[] boos2 = {false,false,false};
+			
+			//boos3 = {false,true,true};
+			boos3 = new boolean[3];
+			
+			
+			/**2 ways to iterate through an array
+			Standard For Loop
+				- the index is important to keep track of
+				- you need to customize the order
+			*/
+			for(int i = 0; i < boos1.length; i++)
+			{
+			System.out.println(boos1[i]);
+			}
+			/**
+			 For each Loop
+			 	- index is not important
+			 	- you don't need to customize
+			 	- automatically assigns a handle -> identifier
+			 */
+			for(boolean b: boos1)
+			{
+				System.out.println(b);
+			}
+			
+			//object arrays
+			String[] someStrings1 = new String[3];
+			String[] someStrings2 = {"a","b","c"};
+			
+			//repetitive
+			someStrings1[0] = "a";
+			someStrings1[1] = "b";
+			someStrings1[2] = "c";
+			
+			for(int i = 0; i <someStrings1.length;i++)
+			{
+				someStrings1[i] = "a new String";
+			}
+			
+			for (String s:someStrings1)
+			{
+				System.out.println(s);
+			}
 		}
 }
