9b3956d2cde96a20b28789fc50034b09cea2f4eb
Student8 <Student8@10.8.33.161>
2016-10-14 13:32:47 -0400

    minor changes

---
+++ Period 8 Java/src/arrays/ArraysPractice.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class ArraysPractice 
+{
+		public static void main(String[] args) 
+		{
+			/*arrays are kind of like lists
+			 * have fixed lengths
+			 * start at index 0
+			 * ex. Student[Joe, Jimmy, Bob] Index [0] = Joe
+			 * All items in an array MUST be an object
+			 * if you want to put different primitive types into an array, you must use their wrapper class
+			 */
+			
+			//2 different ways to instantiate an array
+			boolean[] boos1 = new boolean[3];
+			//can ONLY be done at the declaration because it sets size and content
+			boolean[] boos2 = {false,false,false};
+		}
+}
