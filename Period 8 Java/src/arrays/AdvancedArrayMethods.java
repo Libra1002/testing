2452467b2d46e3665576b00032682e96967520e3
Student8 <Student8@10.8.33.161>
2016-10-21 14:03:20 -0400

    random shuffling method

--- Period 8 Java/src/arrays/AdvancedArrayMethods.java
+++ Period 8 Java/src/arrays/AdvancedArrayMethods.java
@@ -9,21 +9,28 @@ public class AdvancedArrayMethods
 		swap(array,0,array.length - 1);
 		
 	}
-	private static void swap(String[] arr, int a, int b)
+	private static void shuffle(Object [] array)
 	{
-		String placeHolder = arr[b];
-		arr[b] = arr[a];
-		arr[a] = placeHolder;
+		for (int i = 0; i< array.length;i++)
+		{
+			int random  = (int)(Math.random()*6);
+			swap(array,i,random);
+		}
+	}
+	private static void swap(Object[] array2, int a, int b)
+	{
+		String placeHolder = (String) array2[b];
+		array2[b] = array2[a];
+		array2[a] = placeHolder;
 	}
 	private static void methodA(int[] someArray) 
 	{
 		int[] newArray = new int[someArray.length];
 	}
-	
 	public static void copyArray(int[] original, int[] target)
 	{
 		target = original;
-	}
+	
 	/*
 		if(original.length == target.lenth)
 		{
@@ -37,4 +44,5 @@ public class AdvancedArrayMethods
 				System.out.println("ERROR: tried to copy arrays of two different lengths");
 			}
 	*/
+	}
 }
