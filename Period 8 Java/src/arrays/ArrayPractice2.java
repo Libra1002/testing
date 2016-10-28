0b682a3302fb51096f0e1c61b97eac5f63bc0764
Student8 <Student8@10.8.33.161>
2016-10-28 13:24:39 -0400

    Sieve of Eratosthenes?

--- Period 8 Java/src/arrays/ArrayPractice2.java
+++ Period 8 Java/src/arrays/ArrayPractice2.java
@@ -4,22 +4,67 @@ public class ArrayPractice2 {
 
 	public static void main(String[] args) 
 	{
-		//how do you time a process
-		long currentTime = System.currentTimeMillis();initializingArraysExample();
-		
-		int[] fiftyNumbers = new int[50];
-		populate(fiftyNumbers);
-		print(fiftyNumbers);
-		randomize(fiftyNumbers,50);
-		print(fiftyNumbers);
-		rollDice(fiftyNumbers,2);
-		print(fiftyNumbers);
-		countResult(fiftyNumbers,3);
-		print(fiftyNumbers);
-		long endTime = System.currentTimeMillis();
-		System.out.println("The process took " + (endTime - currentTime) + " milliseconds.");
+		listPrimes(120);
+//		//how do you time a process
+//		long currentTime = System.currentTimeMillis();initializingArraysExample();
+//		
+//		int[] fiftyNumbers = new int[50];
+//		populate(fiftyNumbers);
+//		print(fiftyNumbers);
+//		randomize(fiftyNumbers,50);
+//		print(fiftyNumbers);
+//		rollDice(fiftyNumbers,2);
+//		print(fiftyNumbers);
+//		countResult(fiftyNumbers,3);
+//		print(fiftyNumbers);
+//		long endTime = System.currentTimeMillis();
+//		System.out.println("The process took " + (endTime - currentTime) + " milliseconds.");
 
 	}
+	
+	private static void listPrimes(int limit)
+	{
+		int lastToCheck = (int)(Math.sqrt(limit));
+		boolean[]numbers = new boolean[limit+1];
+		for(int i = 0; i< limit+1;i++)
+		{
+			numbers[i] = true;
+		}
+		numbers[0] = false;
+		numbers[1] = false;
+		for(int prime = 2;prime<=lastToCheck;prime++)
+		{
+			if(numbers[prime])
+			{
+				System.out.println("\n"+prime+" is prime. Crossing off: " );
+				for(int i = (int)(Math.pow(prime, 2));i < limit+1;i+=prime)
+				{
+					System.out.print(i+" ");
+					numbers[i] = false;
+				}
+			}
+		}
+		System.out.println("\n The primes are:" );
+		for(int index = 0;index<numbers.length;index++)
+		{
+			if(numbers[index])
+			{
+				System.out.print(index + " , ");
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	private static void populate(int[] nums) 
 	{
