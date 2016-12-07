0f09539bce5553d6fd14218a7e79a7896a1bea57
Student8 <Student8@10.8.33.161>
2016-12-07 14:04:38 -0500

    New stuffz

---
+++ Period 8 Java/src/gooey/Screen.java
@@ -0,0 +1,32 @@
+package gooey;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public class Screen 
+{
+	private int width;
+	private int height;
+	protected BufferedImage image;
+	public Screen(int width, int height) 
+	{
+		this.width = width;
+		this.height = height;
+		initImage();
+	}
+	private void initImage() 
+	{
+		image = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
+		update();
+	}
+	private void update() 
+	{
+		//this is where you draw stuff
+		Graphics2D g = image.createGraphics();
+		g.drawString("Hello", 40, 40);
+	}
+	public BufferedImage getImage()
+	{
+		return image;
+	}
+}
